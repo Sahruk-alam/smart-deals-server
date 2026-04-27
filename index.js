f65615c9d9217c782e811ecd2597b777011e434f
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-04-27 20:06:40 +0600

    fix vercel routing

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const admin = require("firebase-admin");
 const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
 const serviceAccount = JSON.parse(decoded);
 
+if (!process.env.FIREBASE_SERVICE_KEY) {
+  throw new Error("Missing FIREBASE_SERVICE_KEY in Vercel");
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
